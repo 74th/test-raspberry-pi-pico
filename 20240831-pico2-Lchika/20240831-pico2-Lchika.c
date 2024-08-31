8c8a53349ee353a9744fc2f06203227b9a163401
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-08-31 22:47:55 +0900

    up

--- 20240831-pico2-Lchika/20240831-pico2-Lchika.c
+++ 20240831-pico2-Lchika/20240831-pico2-Lchika.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define LED_PIN PICO_DEFAULT_LED_PIN
+// #define LED_PIN 18
+
 int main()
 {
     stdio_init_all();
 
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
     while (true)
     {
         printf("Hello, world!\n");
         sleep_ms(1000);
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        gpio_put(LED_PIN, 1);
         sleep_ms(1000);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        gpio_put(LED_PIN, 0);
     }
 }
