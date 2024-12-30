5293509cc8e7c08407c45bbb1a2f75b637621b23
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-12-30 02:12:28 +0000

    uart echo

---
+++ 20241027-pico2-uart-echo/20241027-pico2-uart-echo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
+int main()
+{
+    stdio_init_all();
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\r\n");
+
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    while (true)
+    {
+        char c = uart_getc(UART_ID);
+        char buf[100];
+        // uart_puts(UART_ID, "Receive:");
+        sprintf(buf, "Receive: %d\r\n", c);
+        uart_puts(UART_ID, buf);
+        sleep_ms(100);
+    }
+}
